ac1c2f91e8106d4ee7c705a2e207c0f7265ba5e6
Victória Thaísa <victhc03@gmail.com>
2024-11-01 13:38:20 -0300

    ultima atualização

--- app.js
+++ app.js
@@ -1,3 +1,35 @@
+let items = document.querySelectorAll('.slider .item')
+let prevBtn = document.getElementById('prev')
+let nextBtn = document.getElementById('next')
+let lastPosition = items.length - 1
+let firstPosition = 0
+let active = 1
+
+nextBtn.onclick = () =>{
+    active++
+    setSlider()
+}
+
+prevBtn.onclick = () => {
+    active--
+    setSlider()
+}
+
+const setSlider = () => {
+    let oldActive = document.querySelector('.slider .item.active')
+    if(oldActive) oldActive.classList.remove('active')
+    items[active].classList.add('active')
+
+    nextBtn.classList.remove('d-none')
+    prevBtn.classList.remove('d-none')
+
+    if(active == lastPosition) nextBtn.classList.add('d-none')
+    if(active == firstPosition) prevBtn.classList.add('d-none')  
+
+}
+
+setSlider()
+
 const setDiameter = () => {
     let slider = document.querySelector('.slider')
     let widthSlider = slider.offsetWidth
